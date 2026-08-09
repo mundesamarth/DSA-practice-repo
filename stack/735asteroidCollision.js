04ef5dc4e5dd3fb520992c3b4e4d2493dbfe83e2
Sam <munde.samarth28@gmail.com>
2026-08-09 12:46:03 +0100

    735 asteroid

--- stack/735asteroidCollision.js
+++ stack/735asteroidCollision.js
@@ -1,10 +1,37 @@
-asteroids = [5,10,-5]
+asteroids = [5,10,-5];
 
 const asteroidCollision = (asteroids) => {
-    let arr = [];
-   for(let i = 0; i < asteroids.length; i++){
-    console.log(Math.abs(asteroids[i]))
-   }
-}
+  let stack = [];
 
-console.log(asteroidCollision(asteroids))
+  for (let asteroid of asteroids) {
+    let alive = true;
+
+    while (
+      alive &&
+      asteroid < 0 &&
+      stack.length > 0 &&
+      stack[stack.length - 1] > 0
+    ) {
+        const top = stack[stack.length-1];
+
+        if(top < Math.abs(asteroid)){
+            stack.pop();
+        }
+        else if(top === Math.abs(asteroid)){
+            stack.pop();
+            alive = false;
+        }
+        else{
+            alive = false;
+        }
+    }
+
+    if(alive){
+        stack.push(asteroid);
+    }
+
+  }
+  return stack;
+};
+
+console.log(asteroidCollision(asteroids));

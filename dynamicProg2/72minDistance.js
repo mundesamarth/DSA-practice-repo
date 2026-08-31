632a1c9003255b41677f55e6b1d2bc7115d66910
Sam <munde.samarth28@gmail.com>
2026-08-31 14:46:39 +0100

    min distance 72

---
+++ dynamicProg2/72minDistance.js
@@ -0,0 +1,29 @@
+let word1 = "horse";
+let word2 = "ros";
+const minDistance = (word1, word2) => {
+  let m = word1.length;
+  let n = word2.length;
+
+  let dp = Array.from({ length: m + 1 }, () => new Array(n + 1).fill(0));
+
+  for (let i = 0; i <= m; i++) {
+    dp[i][0] = i;
+  }
+  for (let j = 0; j <= n; j++) {
+    dp[0][j] = j;
+  }
+
+  for (let i = 1; i <= m; i++) {
+    for (let j = 1; j <= n; j++) {
+      if (word1[i - 1] === word2[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1];
+      } else {
+        dp[i][j] = 1 + Math.min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]);
+      }
+    }
+  }
+
+  return dp[m][n];
+};
+
+console.log(minDistance(word1, word2));

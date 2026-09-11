544d1dfc9db2b0d5f1c6735ca984112e2f9065b8
Sam <munde.samarth28@gmail.com>
2026-09-11 18:30:01 +0100

    15 3sum

---
+++ TwoPointer/152sum.js
@@ -0,0 +1,37 @@
+let nums = [-100,-70,-60,110,120,130,160]
+
+const threeSum = (nums) => {
+  let result = [];
+  nums.sort((a, b) => a - b);
+
+  for (let i = 0; i < nums.length - 2; i++) {
+    if (i > 0 && nums[i] === nums[i - 1]) continue;
+
+    let left = i + 1;
+    let right = nums.length - 1;
+
+    while (left < right) {
+      let sum = nums[i] + nums[left] + nums[right];
+      if (sum > 0) {
+        right--;
+      } else if (sum < 0) {
+        left++;
+      } else {
+        result.push([nums[i], nums[left], nums[right]]);
+
+        left++;
+        right--;
+
+        while (left < right && nums[left] === nums[left - 1]) {
+          left++;
+        }
+        while (left < right && nums[right] === nums[right + 1]) {
+          right--;
+        }
+      }
+    }
+  }
+  return result;
+};
+
+console.log(threeSum(nums));
